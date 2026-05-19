ebe8aa2ef854e599aac5d44978d7112dc78cd879
Nicolas Redfern <nick@redfern.org.uk>
2026-05-19 11:46:34 +0100

    Stop on error
    Stop on error

--- Disable-SQL.ps1
+++ Disable-SQL.ps1
@@ -6,37 +6,42 @@
 	Saves list of running SQL services to .\running-SQL.csv
 .NOTES
 	Author: Nicolas Redfern
-	Date: 2026-04-30
-	Version: 1.0
+	Date: 2026-05-19
+	Version: 1.1
 	No AI was used in the creation of this script.
 .LINK
 	Use Enable-SQL.ps1 to re-enable SQL services
 #>
-"_____________________________________________________________________"
-$SQL_services = Get-Service *SQL*
-"SQL services on "+$env:COMPUTERNAME
+try{
+	"_____________________________________________________________________"
+	$SQL_services = Get-Service *SQL*
+	"SQL services on "+$env:COMPUTERNAME
 
-$SQL_services
-$running_SQL_services = $SQL_services | Where-Object Status -eq 'running'
-$number_to_stop = $running_SQL_services.count
+	$SQL_services
+	$running_SQL_services = $SQL_services | Where-Object Status -eq 'running'
+	$number_to_stop = $running_SQL_services.count
 
-if ($number_to_stop -eq 0){
-	"NO SQL SERVICES TO STOP. Were they already stopped?"
-} elseif ($number_to_stop -In 1..20){
-	
-	""
-	Write-Host("Would you like to disable the following " + $number_to_stop + " running services")
-	$running_SQL_services
-	$confirmation = Read-Host("(y/N)?")
+	if ($number_to_stop -eq 0){
+		"NO SQL SERVICES TO STOP. Were they already stopped?"
+	} elseif ($number_to_stop -In 1..20){
+		
+		""
+		Write-Host("Would you like to disable the following " + $number_to_stop + " running services")
+		$running_SQL_services
+		$confirmation = Read-Host("(y/N)?")
 
-	if ($confirmation -eq 'y'){
-		$running_SQL_services| Export-Csv .\running-SQL.csv
-		"Disabling SQL services:"
-		Get-Service -Name $running_SQL_services.Name | Stop-Service -Force -PassThru
-		Get-Service -Name $running_SQL_services.Name | Set-Service -StartupType Disabled
+		if ($confirmation -eq 'y'){
+			$running_SQL_services| Export-Csv .\running-SQL.csv
+			"Disabling SQL services:"
+			Get-Service -Name $running_SQL_services.Name | Stop-Service -Force -PassThru
+			Get-Service -Name $running_SQL_services.Name | Set-Service -StartupType Disabled
+		} else {
+			"No action taken"
+		}
 	} else {
-		"No action taken"
+		"TOO MANY SERVICES, either something went wrong or server has vast number of instances. Double check services to be stopped and count, and manually change the -In 1.20 line if larger range required"
 	}
-} else {
-	"TOO MANY SERVICES, either something went wrong or server has vast number of instances. Double check services to be stopped and count, and manually change the -In 1.20 line if larger range required"
+}
+catch{
+	"AN ERROR OCCURED"
 }
